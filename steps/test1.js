3ec343f89e1dd8e86d121b5b7308cd581f67bb79
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2019-10-24 06:08:19 +0700

    test steps

--- steps/test1.js
+++ steps/test1.js
@@ -26,29 +26,5 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
     })
   })
 
-  describe('products', async () => {
-    let result, productCount
-
-    before(async () => {
-      result = await marketplace.createProduct('iPhone X', web3.utils.toWei('1', 'Ether'), { from: seller })
-      productCount = await marketplace.productCount()
-    })
-
-    it('creates products', async () => {
-      // SUCCESS
-      assert.equal(productCount, 1)
-      const event = result.logs[0].args
-      assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
-      assert.equal(event.name, 'iPhone X', 'name is correct')
-      assert.equal(event.price, '1000000000000000000', 'price is correct')
-      assert.equal(event.owner, seller, 'owner is correct')
-      assert.equal(event.purchased, false, 'purchased is correct')
-
-      // FAILURE: Product must have a name
-      await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
-      // FAILURE: Product must have a price
-      await await marketplace.createProduct('iPhone X', 0, { from: seller }).should.be.rejected;
-    })
-
 
 })
